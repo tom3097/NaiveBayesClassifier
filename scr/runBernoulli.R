dd9046716a7ad0a8325cc5381b5fac7287be442b
Tomasz Bochenski <tbochens@gmail.com>
2019-01-20 20:06:46 +0100

    tests
    Signed-off-by: Tomasz Bochenski <tbochens@gmail.com>

---
+++ scr/runBernoulli.R
@@ -0,0 +1,48 @@
+#FIXME: this package should load while loading naiveBayes package as a dependency
+if (!require("ramify")) {
+  install.packages("ramify", repo=NULL, type="source")
+}
+if (!require("naiveBayes")) {
+  install.packages("naiveBayes", repo=NULL, type="source")
+}
+if (!require("e1071")) {
+  install.packages("e1071", repo=NULL, type="source")
+}
+
+library(naiveBayes)
+library(e1071)
+
+source("utils/metrics.R")
+
+
+main <- function() {
+  args = commandArgs(trailingOnly=TRUE)
+  if (length(args) != 1) {
+  	cat("Please provide one required parameter (no more)")
+  	return(-1)
+  }
+
+  load(paste0("../data/", args))
+
+  cat("Testing BERNOULLI naive bayes\n\n")
+
+  cat("e1071 naive bayes:\n")
+  startTime <- Sys.time()
+  naiveBayesModel=naiveBayes(Y ~., data=bagOfWordsBinary, laplace = 1)
+  NBPredictions=predict(naiveBayesModel,bagOfWordsBinary)
+  endTime <- Sys.time()
+  cat(paste("Accuracy:", accuracyScore(NBPredictions, bagOfWordsBinary$Y), "\n"))
+  cat(paste("Duration:", endTime - startTime, "\n"))
+
+  cat("\n")
+
+  cat("custom naive bayes:\n")
+  startTime <- Sys.time()
+  bnb <- bernoulliNaiveBayes(Y ~., bagOfWordsBinary)
+  bnbPredictions = predict(bnb, bagOfWordsBinary)
+  endTime <- Sys.time()
+  cat(paste("Accuracy:", accuracyScore(bnbPredictions, bagOfWordsBinary$Y), "\n"))
+  cat(paste("Duration:", endTime - startTime, "\n"))
+}
+
+main()
