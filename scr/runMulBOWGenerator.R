d10937a152ee3794618ef24a8f2b88d8f24e8e3a
Tomasz Bochenski <tbochens@gmail.com>
2019-01-21 06:24:11 +0100

    project
    Signed-off-by: Tomasz Bochenski <tbochens@gmail.com>

---
+++ scr/runMulBOWGenerator.R
@@ -0,0 +1,34 @@
+source("transformer/transformer.R")
+
+main <- function() {
+  args <- commandArgs(trailingOnly=TRUE)
+  if (length(args) < 2 || length(args) > 3) {
+  	cat("Please provide two or three required parameter (no more)")
+  	return(-1)
+  }
+
+  dataset <- args[1]
+  dictSize <- as.numeric(args[2])
+  removeHeaders <- FALSE
+  if (length(args) == 3 && args[3] == "removeHeaders") {
+    removeHeaders <- TRUE
+  }
+
+  wordsFreq <- wordsDict(paste0("../data/transitional/", dataset, ".csv"), dictSize)
+  bagOfWords <- transToBOW(paste0("../data/raw/", dataset), wordsFreq$word, FALSE, removeHeaders)
+
+  abbr <- ""
+  if (dataset == "20_newsgroups") {
+    abbr <- "20NG"
+  }
+  if (dataset == "C50") {
+    abbr <- "C50"
+  }
+  if (dataset == "emotions") {
+    abbr <- "EMO"
+  }
+
+  save(bagOfWords, file = paste0("../data/processed/", abbr, "_", dictSize, "_M_.Rda"));
+}
+
+main()
--- scr/transformer/transformer.R
+++ scr/transformer/transformer.R
@@ -36,10 +36,8 @@ wordsFreq <- function(dirPath, removeHeaders = FALSE) {
   for(category in categories) {
     categoryPath <- paste(dirPath, toString(category), sep = "/");
     categoryFiles <- list.files(categoryPath);
-    for(catFile in categoryFiles)
-    {
+    for(catFile in categoryFiles) {
       filePath <- paste(categoryPath, toString(catFile), sep = "/");
-      
       data <- readLines(filePath);
             
       if (removeHeaders == TRUE) {
@@ -75,4 +73,81 @@ wordsFreq <- function(dirPath, removeHeaders = FALSE) {
   
   freqs <- resultFrame[rev(order(resultFrame$freq)), ]
   return(freqs)
+}
+
+# Generates words dictionary with the given size based on the given frequency file.
+wordsDict <- function(file, N) {
+  wordsFreq <- read.csv(file = file, header = TRUE, sep = ",")
+  wordsFreq <- wordsFreq[, 2:3]
+  
+  dict <- wordsFreq[1:N, ]
+  return(dict)
+}
+
+# Transforms given dataset to bag of words using given dictionary.
+transToBOW <- function(dirPath, words, binaryFreq, removeHeaders = FALSE) {
+  bagOfWords <- data.frame(matrix(ncol = length(words)+1, nrow = 0));
+  classID <- 1;
+  
+  categories <- list.files(dirPath);
+  for(category in categories) {
+    categoryPath <- paste(dirPath, toString(category), sep = "/");
+    categoryFiles <- list.files(categoryPath);
+    for(catFile in categoryFiles) {
+      filePath <- paste(categoryPath, toString(catFile), sep = "/");
+      data <- readLines(filePath);
+
+      if (removeHeaders == TRUE) {
+        flag <- FALSE;
+        dirtyData <- data
+        data <- character();
+        iter <- 0;
+        
+        for(line in dirtyData) {
+          
+          if(!isTRUE(flag) && (!str_detect(line, ""))) {
+            flag <- TRUE;
+          }
+          
+          if(!isTRUE(flag)) {
+            next;
+          }
+          else {
+            iter <- iter + 1;
+            data[iter] <- line;
+          }
+        }
+      }
+      
+      dataFrame <- tmMapTransform(data)
+      
+      freqVec <- c()
+      i <- 1
+      for(w in words) {
+        if(w %in% dataFrame$word) {
+          freqVec[i] <- dataFrame[w, ]$freq;
+          if (binaryFreq == TRUE) {
+            freqVec[i] <- 1
+          }
+        }
+        else {
+          freqVec[i] <- 0;
+        }
+        i <- i + 1;
+      }
+      freqVec[i] <- classID;
+      bagOfWords[nrow(bagOfWords)+1, ] <- freqVec;
+    }
+    
+    classID <- classID + 1;
+  }
+  
+  words <- c(levels(words)[words], 'Y')
+  colnames(bagOfWords) <- words;
+  
+  for (column  in colnames(bagOfWords)) {
+    bagOfWords[, column] <- as.factor(bagOfWords[, column]);
+  }
+  
+  return(bagOfWords)
 }
